9d286dd2069c3f2c00498850368539a87aedc2da
pleszkoactvf <43756310+pleszkoactvf@users.noreply.github.com>
2018-12-21 10:06:34 -0500

    Add files via upload

---
+++ Lesson11/problem1.py
@@ -0,0 +1,13 @@
+print('-'*65)
+print('Iz iT BrIcK BoT?')
+print()
+temp = input('What is the temperature outside? ')
+
+temp = int(temp)
+
+if temp <= 50:
+	print('ItZ bRIck my guy, wear ya jacket')
+
+else:
+	print('Itz not bRIck, enjoy ya day.')
+	
